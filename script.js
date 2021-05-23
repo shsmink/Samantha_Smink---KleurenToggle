5e9be6569fc46c8d937d853e2909c242f1ccdbc2
shsmink <79309340+shsmink@users.noreply.github.com>
2021-05-23 16:57:22 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,59 @@
+const toggleMenu = document.querySelector(".menu__icon");
+const showMenu = document.querySelector(".menu__items");
+
+toggleMenu.addEventListener("mouseover", function(){
+  showMenu.classList.toggle("open-menu")
+});
+
+const bodyBackground = document.querySelector("body");
+
+const changeToGrey = document.querySelector(".background__grey");
+const changeToRed = document.querySelector(".background__red");
+const changeToOrange = document.querySelector(".background__orange");
+const changeToPurple = document.querySelector(".background__purple");
+const changeToGreen = document.querySelector(".background__green");
+
+changeToGrey.addEventListener("click", function(){
+  showMenu.classList.toggle("open-menu")
+  bodyBackground.classList.remove("background__red")
+  bodyBackground.classList.remove("background__orange")
+  bodyBackground.classList.remove("background__purple")
+  bodyBackground.classList.remove("background__green")
+  bodyBackground.classList.toggle("background__grey")
+});
+
+changeToRed.addEventListener("click", function(){
+  showMenu.classList.toggle("open-menu")
+  bodyBackground.classList.remove("background__grey")
+  bodyBackground.classList.remove("background__orange")
+  bodyBackground.classList.remove("background__purple")
+  bodyBackground.classList.remove("background__green")
+  bodyBackground.classList.toggle("background__red")
+});
+
+changeToOrange.addEventListener("click", function(){
+  showMenu.classList.toggle("open-menu")
+  bodyBackground.classList.remove("background__grey")
+  bodyBackground.classList.remove("background__red")
+  bodyBackground.classList.remove("background__purple")
+  bodyBackground.classList.remove("background__green")
+  bodyBackground.classList.toggle("background__orange")
+});
+
+changeToPurple.addEventListener("click", function(){
+  showMenu.classList.toggle("open-menu")
+  bodyBackground.classList.remove("background__grey")
+  bodyBackground.classList.remove("background__red")
+  bodyBackground.classList.remove("background__orange")
+  bodyBackground.classList.remove("background__green")
+  bodyBackground.classList.toggle("background__purple")
+});
+
+changeToGreen.addEventListener("click", function(){
+  showMenu.classList.toggle("open-menu")
+  bodyBackground.classList.remove("background__grey")
+  bodyBackground.classList.remove("background__red")
+  bodyBackground.classList.remove("background__orange")
+  bodyBackground.classList.remove("background__purple")
+  bodyBackground.classList.toggle("background__green")
+});
